cdb9f161b36ab6ef65e410e5dc5a45f9e6da1e44
JiukOn <78093035+JiukOn@users.noreply.github.com>
2022-01-14 20:34:58 -0300

    Update people.sql

--- people.sql
+++ people.sql
@@ -2,10 +2,13 @@ CREATE TABLE people (
     id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(30) NOT NULL,
     birth DATE
-)
+);
 
-INSERT INTO people (name, birth) VALUES ('Um Um de One', '1973-02-25')
-INSERT INTO people (name, birth) VALUES ('Ace de Portgas', '1999-01-01')
-INSERT INTO people (name, birth) VALUES ('Takemichi Hanagaki', '1991-06-25')
+INSERT INTO people (name, birth) VALUES ('Um Um de One', '1973-02-25');
+INSERT INTO people (name, birth) VALUES ('Ace de Portgas', '1999-01-01');
+INSERT INTO people (name, birth) VALUES ('Takemichi Hanagaki', '1991-06-25');
 
-UPDATE people SET name = 'Thanos' WHERE id = 1
+UPDATE people SET name = 'Thanos' WHERE id = 1;
+
+SELECT * FROM people WHERE id = 2;
+DELETE FROM people WHERE id = 2;
