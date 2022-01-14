0c7618ca6aee92b7655a7b9f490bc3308d8f32a1
JiukOn <78093035+JiukOn@users.noreply.github.com>
2022-01-14 19:07:49 -0300

    Add files via upload

---
+++ people.sql
@@ -0,0 +1,9 @@
+CREATE TAVLE people (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+    birth DATE
+)
+
+INSERT INTO people (name, birth) VALUES ('Um Um de One', '2001-01-01')
+INSERT INTO people (name, birth) VALUES ('Ace de Portgas', '1999-01-01')
+INSERT INTO people (name, birth) VALUES ('Takemichi Hanagaki', '1991-06-25')
