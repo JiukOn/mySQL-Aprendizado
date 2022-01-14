45044ff38cf8758f34c012e63eee9900300e3219
JiukOn <78093035+JiukOn@users.noreply.github.com>
2022-01-14 19:53:08 -0300

    Update people.sql

--- people.sql
+++ people.sql
@@ -4,6 +4,8 @@ CREATE TABLE people (
     birth DATE
 )
 
-INSERT INTO people (name, birth) VALUES ('Um Um de One', '2001-01-01')
+INSERT INTO people (name, birth) VALUES ('Um Um de One', '1973-02-25')
 INSERT INTO people (name, birth) VALUES ('Ace de Portgas', '1999-01-01')
 INSERT INTO people (name, birth) VALUES ('Takemichi Hanagaki', '1991-06-25')
+
+UPDATE people SET name = 'Thanos' WHERE id = 1
